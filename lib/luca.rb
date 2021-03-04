889f184b1cf5a92ec47d8e5672f6056997f65978
Chuma Takahiro <chuma@unicorn.limited>
2021-03-04 23:06:12 +0900

    initial implement 消費税簡易課税計算

---
+++ lib/luca.rb
@@ -0,0 +1,2 @@
+module Luca
+end
--- lib/luca/jp/syouhizei.rb
+++ lib/luca/jp/syouhizei.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 require 'luca_book'
+require 'luca_support'
 require 'luca_record/dict'
+require 'luca_support/config'
 
 module Luca
   module Jp
     class Syouhizei
       def initialize(from_year, from_month, to_year = from_year, to_month = from_month)
         @dict = LucaRecord::Dict.load('base.tsv')
-        @state = LucaBook::State(from_year, from_month, to_year, to_month)
+        @state = LucaBook::State.range(from_year, from_month, to_year, to_month)
       end
 
       # TODO: 軽減税率売上の識別
       #
       def kani
-        sales = @state.pl.dig('A0')
+        税率 = 10 # percent
+        @state.pl
+        sales = @state.pl_data.dig('A0')
+        tax_amount = sales * 10 / 100
+        みなし仕入額 = tax_amount * みなし仕入率(LucaSupport::CONFIG.dig('jp', 'syouhizei_kubun')) / 100
+        税額 = LucaSupport::Code.readable(tax_amount - みなし仕入額)
       end
 
       private
